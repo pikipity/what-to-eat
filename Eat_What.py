8866303e305d9b7a5150eeffe8126d57d4ee3569
pikipity <wangze19910407@gmail.com>
2013-05-21 21:14:23 +0800

    v1.0 finished
    version 1.0 完成
    1. 随机抽取餐厅，并提供餐厅详�
    2. 可以自动抓取takeaway Macao数据
    3. 可以设定自己所在区域

--- Eat_What.py
+++ Eat_What.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import Tkinter
+import ttk
 import string
 import random
 import time
@@ -25,6 +26,8 @@ WrongMessage=u"网络数据抓取错误，使用上次保留数据"
 
 icon="icon\\eat.ico"
 
+MaxTryTimes=800
+
 ThingDefault=u"今天晚上吃神马？"
 
 StartButtonString_start=u"开始"
@@ -37,8 +40,16 @@ AboutButtonString=u"关于作者"
 StationDefault=u'点击“开始”或按“回车”，开始随机抽取'
 Station_After_Start=u'点击“停止”或按“回车”，结束随机抽取'
 Station_After_Stop=u'抽取结束，点击餐厅名可以打开外卖单欧（需联网）'
+Wrong_Start_no_DataFile=u'加载数据失败，连接好网络之后，请点击“停止”后再次点击“开始”'
 
 Station_Setting_Before_Stop=u"先停下好吗？"
+SetLocationWrong=u"地点选择错误，将使用上次使用的地点 -- "
+
+SetLocationLabelString=u"选择你所在的地方："
+LocationDefault=u"氹仔"
+StartWebDefault=u"http://takeaway.happymacao.com/other/1"
+ControlSaveButtonString=u"保存"
+ControlCloseButtonString=u"关闭"
 
 EatingNameDefault=u'仅提供 Takeaway Macao 上的餐厅'
 EatingWebDefault=mainhost
@@ -46,6 +57,7 @@ EatingTeleDefault=u'有任何意见或建议可以联系我 -- pikipity'
 EatingNoteDefault=u'我的联系方式：'
 EatingHourDefault=u'pikipity@gmail.com'
 EatingAddressDefault=u'pikipity.github.io'
+DefaultResInfo=u'歡迎致電查詢'
 ###############################################################
 
 ########### 进程间传递参量 ######################################
@@ -54,6 +66,9 @@ StartSwitch.put(0)
 
 EatingWebStore=Queue.Queue(maxsize=1)
 EatingWebStore.put(mainhost)
+
+SetLoad=Queue.Queue(maxsize=1)
+SetLoad.put(0)
 ###############################################################
 
 def GetAndSave_name(Url):
@@ -62,7 +77,10 @@ def GetAndSave_name(Url):
         reqopen=urllib2.urlopen(req)
         Html=reqopen.read()
     except:
-        Station.set(WrongMessage)
+        if os.path.isfile(DataFile):
+            Station.set(WrongMessage)
+        else:
+            Station.set(Wrong_Start_no_DataFile)
     else:
         analysis=BeautifulSoup(Html)
         PageNumber=analysis.find('span',attrs={'id':'pagenum'})
@@ -74,7 +92,10 @@ def GetAndSave_name(Url):
                 reqopen=urllib2.urlopen(req)
                 Html=reqopen.read()
             except:
-                Station.set(WrongMessage)
+                if os.path.isfile(DataFile):
+                    Station.set(WrongMessage)
+                else:
+                    Station.set(Wrong_Start_no_DataFile)
             else:
                 analysis=BeautifulSoup(Html)
                 ana1=analysis.findAll('li')
@@ -92,11 +113,11 @@ def GetAndSave_name(Url):
                         try:
                             ResNote=ana2.find('p',attrs={'id':'note'}).string.encode('utf-8')
                         except:
-                            ResNote='         '
+                            ResNote=DefaultResInfo.encode('utf-8')
                         try:
                             ResHour=ana2.find('p',attrs={'id':'hours'}).string.encode('utf-8')
                         except:
-                            ResHour='         '
+                            ResHour=DefaultResInfo.encode('utf-8')
                         try:
                             ResAddress=ana2.find('p',attrs={'id':'address'}).string.encode('utf-8')
                         except:
@@ -113,19 +134,41 @@ def GetAndSave_name(Url):
 
 def GetResInfo():
     while(1):
+        SetLoad.get()
+        SetLoad.put(1)
         print "I am started"
         try:
             os.remove(DataFile)
         except:
             pass
-        StartWeb=mainhost+'/other/1'
+        try:
+            fp=open(ConfigFile)
+            Configuration=fp.readlines()
+            Configuration[0]=Configuration[0].decode('utf-8')
+            StartWeb=Configuration[0][Configuration[0].rfind(': ')+2:\
+                                        Configuration[0].rfind('\n')]
+            fp.close()
+        except:
+            StartWeb=StartWebDefault
         GetAndSave_name(StartWeb)
+        print "I am finished. Waiting"
+        SetLoad.get()
+        SetLoad.put(0)
         time.sleep(30)
+        SetLoadValue=SetLoad.get()
+        SetLoad.put(SetLoadValue)
+        while(SetLoadValue==1):
+            SetLoadValue=SetLoad.get()
+            SetLoad.put(SetLoadValue)
 
 def FirstReadDataFile():
     DataFileOK=0
+    TryTimes=0
     while(DataFileOK==0):
+        TryTimes=TryTimes+1
+        print TryTimes
         Thing.set(u"稍等，下载数据中...")
+        Station.set(u"稍等，下载数据中...")
         try:
             fp=open(DataFile,'r')
             lines=fp.readlines()
@@ -137,7 +180,13 @@ def FirstReadDataFile():
                 DataFileOK=1
             else:
                 DataFileOK=0
-    return lines
+        if TryTimes==MaxTryTimes:
+            break
+    if TryTimes==MaxTryTimes:
+        Thing.set(u"下载数据错误")
+        return TryTimes
+    else:
+        return lines
 
 def ReadDataFile(Oddlines):
     try:
@@ -156,9 +205,11 @@ def ReadDataFile(Oddlines):
 def randomstring():
     Beginning=StartSwitch.get()
     StartSwitch.put(Beginning)
+    FirstLoadDataThreading=threading.Thread(target=SetLoadFunction)
+    FirstLoadDataThreading.start()
     lines=FirstReadDataFile()
     Station.set(Station_After_Start)
-    while(Beginning==1):
+    while(Beginning==1 and lines<>MaxTryTimes):
         num=random.randrange(1,len(lines))
         Eating=lines[num]
         if Eating.rfind("@")==-1:
@@ -183,17 +234,20 @@ def randomstring():
             lines=ReadDataFile(lines)
         Beginning=StartSwitch.get()
         StartSwitch.put(Beginning)
-    NowEatingString=Thing.get()
-    Thing.set(NowEatingString[0:len(NowEatingString)-1]+u"!")
-    EatingNameVS.set(u'餐厅名：'+EatingName.decode('utf-8'))
-    EatingWebVS.set(u'菜单：'+EatingWeb.decode('utf-8'))
-    EatingWebStore.get()
-    EatingWebStore.put(EatingWeb)
-    EatingTeleVS.set(u'电话：'+EatingTele.decode('utf-8'))
-    EatingNoteVS.set(u'注释：'+EatingNote.decode('utf-8'))
-    EatingHourVS.set(u'营业时间：'+EatingHour.decode('utf-8'))
-    EatingAddressVS.set(u'地址：'+EatingAddress.decode('utf-8'))
-    Station.set(Station_After_Stop)
+    if lines==MaxTryTimes:
+        Station.set(Wrong_Start_no_DataFile)
+    else:
+        NowEatingString=Thing.get()
+        Thing.set(NowEatingString[0:len(NowEatingString)-1]+u"!")
+        EatingNameVS.set(u'餐厅名：'+EatingName.decode('utf-8'))
+        EatingWebVS.set(u'菜单：'+EatingWeb.decode('utf-8'))
+        EatingWebStore.get()
+        EatingWebStore.put(EatingWeb)
+        EatingTeleVS.set(u'电话：'+EatingTele.decode('utf-8'))
+        EatingNoteVS.set(u'注释：'+EatingNote.decode('utf-8'))
+        EatingHourVS.set(u'营业时间：'+EatingHour.decode('utf-8'))
+        EatingAddressVS.set(u'地址：'+EatingAddress.decode('utf-8'))
+        Station.set(Station_After_Stop)
 
 
 def OpenEatingWeb(event="None"):
@@ -223,7 +277,133 @@ def SetWindow():
     if(Beginning==1):
         tkMessageBox.showwarning(u'呵呵',Station_Setting_Before_Stop)
     else:
-        win32api.ShellExecute(0,'open','notepad.exe',DataFile,'',1)
+        global SetWin
+        SetWin=Tkinter.Toplevel()
+        SetWin.iconbitmap(icon)
+        SetWin.title(SettingButtonString+' -- '+SoftwareName)
+        #选择地点
+        LocationFrame=Tkinter.Frame(SetWin)
+        LocationFrame.pack()
+        LocationLabel=Tkinter.Label(LocationFrame,text=SetLocationLabelString\
+                                    ,font="Times,30,block")
+        LocationLabel.pack(side="left")
+        global Location
+        Location=Tkinter.StringVar()
+        LocationList=[u'關閘台山',\
+                      u'筷子基 / 青州',\
+                      u'黑沙環新填海區',\
+                      u'祐漢 / 龍園 / 錦繡',\
+                      u'美副將 / 雅廉訪',\
+                      u'新僑 / 三盞燈 / 塔石',\
+                      u'南灣 / 水坑尾 / 東望洋',\
+                      u'新馬路 / 白鴿巢',\
+                      u'皇朝 / 新口岸',\
+                      u'媽閣 / 下環',\
+                      LocationDefault]
+        LocationChoose=ttk.Combobox(LocationFrame,text=Location\
+                                    ,value=LocationList\
+                                    ,font="Times,30,block")
+        LocationChoose.pack(side="left")
+        try:
+            fp=open(ConfigFile,'r')
+            Configuration=fp.readlines()
+            Configuration[1]=Configuration[1].decode('utf-8')
+            LocationBeforeChoosed=Configuration[1][Configuration[1].rfind(u': ')+2:\
+                                        len(Configuration[1])]
+            fp.close()
+        except:
+            LocationBeforeChoosed=LocationDefault
+        Location.set(LocationBeforeChoosed)
+        #Button
+        SetControlFrame=Tkinter.Frame(SetWin)
+        SetControlFrame.pack()
+        SetControlSaveButton=Tkinter.Button(SetControlFrame,\
+                                            text=ControlSaveButtonString,\
+                                            command=ControlSaveFunction)
+        SetControlSaveButton.pack(side="left")
+        SetControlCloseButton=Tkinter.Button(SetControlFrame,\
+                                             text=ControlCloseButtonString,\
+                                             command=ControlCloseFunction)
+        SetControlCloseButton.pack(side="left")
+
+def ControlSaveFunction():
+    LocationChoosed=Location.get()
+    if LocationChoosed==u'關閘台山':
+        StartWeb="http://takeaway.happymacao.com/districtA/4"
+    elif LocationChoosed==u'筷子基 / 青州':
+        StartWeb="http://takeaway.happymacao.com/districtA/3"
+    elif LocationChoosed==u'黑沙環新填海區':
+        StartWeb="http://takeaway.happymacao.com/districtA/1"
+    elif LocationChoosed==u'祐漢 / 龍園 / 錦繡':
+        StartWeb="http://takeaway.happymacao.com/districtA/5"
+    elif LocationChoosed==u'美副將 / 雅廉訪':
+        StartWeb="http://takeaway.happymacao.com/districtA/2"
+    elif LocationChoosed==u'新僑 / 三盞燈 / 塔石':
+        StartWeb="http://takeaway.happymacao.com/districtB/1"
+    elif LocationChoosed==u'南灣 / 水坑尾 / 東望洋':
+        StartWeb="http://takeaway.happymacao.com/districtB/2"
+    elif LocationChoosed==u'新馬路 / 白鴿巢':
+        StartWeb="http://takeaway.happymacao.com/districtB/3"
+    elif LocationChoosed==u'皇朝 / 新口岸':
+        StartWeb="http://takeaway.happymacao.com/other/2"
+    elif LocationChoosed==u'媽閣 / 下環':
+        StartWeb="http://takeaway.happymacao.com/other/3"
+    elif LocationChoosed==LocationDefault:
+        StartWeb="http://takeaway.happymacao.com/other/1"
+    else:
+        fp=open(ConfigFile,'r')
+        Configuration=fp.readlines()
+        Configuration[0]=Configuration[0].decode('utf-8')
+        Configuration[1]=Configuration[1].decode('utf-8')
+        LocationChoosed=Configuration[1][Configuration[1].rfind(': ')+2:\
+                                    Configuration[1].rfind('\n')]
+        StartWeb=Configuration[0][Configuration[0].rfind(': ')+2:\
+                                    Configuration[0].rfind('\n')]
+        fp.close()
+        tkMessageBox.showwarning(u'地点设置错误',SetLocationWrong+LocationChoosed)
+    try:
+        os.remove(ConfigFile)
+    except:
+        pass
+    Configuration=[]
+    fp=open(ConfigFile,'w')
+    Configuration.append(u'StartWeb: '+StartWeb+'\n')
+    Configuration.append(u'Location: '+LocationChoosed+'\n')
+    Configuration[0]=Configuration[0].encode('utf-8')
+    Configuration[1]=Configuration[1].encode('utf-8')
+    fp.writelines(Configuration)
+    fp.close()
+    SetLoadThreading=threading.Thread(target=SetLoadFunction)
+    SetLoadThreading.start()
+    SetWin.destroy()
+
+def SetLoadFunction():
+    SetLoadValue=SetLoad.get()
+    SetLoad.put(SetLoadValue)
+    while(SetLoadValue==1):
+        SetLoadValue=SetLoad.get()
+        SetLoad.put(SetLoadValue)
+    SetLoad.get()
+    SetLoad.put(1)
+    try:
+        os.remove(DataFile)
+    except:
+        pass
+    fp=open(ConfigFile)
+    Configuration=fp.readlines()
+    Configuration[0]=Configuration[0].decode('utf-8')
+    StartWeb=Configuration[0][Configuration[0].rfind(': ')+2:\
+                                Configuration[0].rfind('\n')]
+    fp.close()
+    GetAndSave_name(StartWeb)
+    SetLoad.get()
+    SetLoad.put(0)
+    
+    
+
+def ControlCloseFunction():
+    SetWin.destroy()
+        
 
         
 def AboutFunction():
@@ -248,6 +428,7 @@ def AboutFunction():
 def OpenBlog(event="None"):
     webbrowser.open("pikipity.github.io")
     
+###########################################################################
 
 GetResInfoThreading=threading.Thread(target=GetResInfo)
 GetResInfoThreading.start()
